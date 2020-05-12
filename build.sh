1c58c2947db377133472c3aa9eae559dded18151
Piotr Jaczewski <piotr.jaczewski@rtbhouse.com>
2020-05-12 22:11:51 +0200

    applesoft cartridge added

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 xa -W -C -v -O ASCII -c src/cartridge.xa -l cartridge.label -o cartridge.bin
+xa -W -C -v -O ASCII -c src/cartridge_applesoft.xa -l cartridge_applesoft.label -o cartridge_applesoft.bin
