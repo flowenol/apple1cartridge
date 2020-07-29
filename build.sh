dc5ad5f3ac7dcedb098d7366e2aa0fdd353330b1
Piotr Jaczewski <piotr.jaczewski@rtbhouse.com>
2020-07-29 11:12:19 +0200

    cartridge ram & rom banking

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
 xa -W -C -v -O ASCII -c src/cartridge.xa -l cartridge.label -o cartridge.bin
-xa -W -C -v -O ASCII -c src/cartridge_applesoft.xa -l cartridge_applesoft.label -o cartridge_applesoft.bin
